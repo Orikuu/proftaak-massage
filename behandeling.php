a80abf8549c99964c0cc9553738210298f587bcd
Unknown <80656@rocteraa-student.nl>
2018-04-23 18:37:58 +0200

    Actually fixed behandelingen.php vh this commit.

--- behandeling.php
+++ behandeling.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8" />
-<meta name="description" content="Portfolio">
+<meta name="description" content="#1 massage praktijk in Helmond.">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Syncopate" />
