816916881d27cf779b6564ca5fa94e91e23585c4
rikudou sennin <79812@rocteraa-student.nl>
2018-04-23 12:07:59 +0200

    Bootstrap thing

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <meta charset="utf-8" />
 <meta name="description" content="Portfolio">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 <link rel="shortcut icon" href="images/OSSY.ico">
 <title>Care</title>
 </head>
