f598e0dfab85e3bf25c5abb301929cc167fc7576
Tim <80656@rocteraa-student.nl>
2018-04-23 17:40:21 +0200

    Fixed vh and white spacing

--- behandeling.php
+++ behandeling.php
@@ -5,6 +5,7 @@
 <meta name="description" content="Portfolio">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Syncopate" />
 <link rel="stylesheet" href="./css/behandeling.css">
 <link rel="shortcut icon" href="img/OSSY.ico">
 <title>Care</title>
@@ -21,8 +22,8 @@
     </div>
   </nav>
 
-  <div class="whitespace mt-1">
-    <h1>Massage</h1>
+  <div class="whitespace mt-3 text-center">
+    <p>Relax Massage</p>
   </div>
   <div id="box1">
     <div class="content">
@@ -30,8 +31,8 @@
     </div>
   </div>
 
-  <div class="whitespace mt-1">
-    <h1>Homopatie</h1>
+  <div class="whitespace mt-3 text-center">
+    <p>Sport Massage</p>
   </div>
   <div id="box2">
     <div class="content">
@@ -39,14 +40,13 @@
     </div>
   </div>
 
-  <div class="whitespace mt-1">
-    <h1>Behandeling</h1>
+  <div class="whitespace mt-3 text-center">
+    <p>Bindweefsel Massage</p>
   </div>
   <div id="box3">
     <div class="content">
       
     </div>
   </div>
-
 </body>
 </html>
