f99a7df4003c5fbb980bec15c78fc42e2e4ff7e9
Tim <80656@rocteraa-student.nl>
2018-05-08 09:46:00 +0200

    Fix smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+//Anchor smooth scrolling
+$(document).ready(function(){
+    //Detect if the clicked object has a href that starts with #.
+    $('a[href^="#"]').on('click',function (e) {
+        var target = this.hash;
+        var $target = $(target);
+        
+        //Scroll and show anchor
+        $('html, body').animate({
+            // - 70 because the fixed header is 70px high. The title would be behind the header which isn't good.
+            'scrollTop': $target.offset().top - 70
+            }, 1500, 'swing', function() {
+                window.location.hash = target;
+            });
+    });
+});
+
 function mainFadeIn() {
     $('.welcome').fadeIn(2000);
     setTimeout(function(){ $('.redirectbtn').fadeIn(2000) }, 200);
@@ -20,12 +37,10 @@ function validateForm() {
 }
 
 document.getElementById('mode').addEventListener('change', function () {
-    if ($(this).prop('checked'))
-    {
+    if ($(this).prop('checked')) {
         $('nav').addClass('navbar-dark bg-dark');
     }
-    else
-    {
+    else {
         $('nav').removeClass('navbar-dark bg-dark');
     }
     let darkThemeEnabled = document.body.classList.toggle('dark-theme');
@@ -35,20 +50,3 @@ document.getElementById('mode').addEventListener('change', function () {
 if (JSON.parse(localStorage.getItem('dark-theme-enabled'))) {
     document.body.classList.add('dark-theme');
 }
-
-//Anchor smooth scrolling
-$(document).ready(function(){
-	//Detect if the clicked object has a href that starts with #.
-	$('a[href^="#"]').on('click',function (e) {
-		var target = this.hash;
-		var $target = $(target);
-		
-		//Scroll and show anchor
-		$('html, body').animate({
-			// - 70 because the fixed header is 70px high. The title would be behind the header which isn't good.
-			'scrollTop': $target.offset().top - 70
-			}, 1500, 'swing', function() {
-				window.location.hash = target;
-			});
-	});
-});
